51b6433ee8b154a42e075f6a2b97166e84ca96ef
Youssef adel <darkmetalxdragon@hotmail.com>
2021-12-09 09:49:03 +0200

    seperate post and filter post controller

--- post-server/src/modules/posts/posts.module.ts
+++ post-server/src/modules/posts/posts.module.ts
@@ -1,4 +1,4 @@
-import { PostsController } from './controllers/posts.controller';
+import { FilterPostsController, PostsController } from './controllers/posts.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Module, Post } from '@nestjs/common';
 import { PostsService } from './services/posts.service';
@@ -6,7 +6,7 @@ import { PostSchema } from './schemas/post.schema';
 
 @Module({
   imports: [MongooseModule.forFeature([{name: 'Post', schema: PostSchema}])],
-  controllers:[PostsController],
+  controllers: [PostsController, FilterPostsController],
   providers: [PostsService]
 })
 export class PostsModule {}
